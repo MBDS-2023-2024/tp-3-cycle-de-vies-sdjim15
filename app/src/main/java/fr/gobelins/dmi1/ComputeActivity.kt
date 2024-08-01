f97e970df6646a8014bb0955292e9484baccb76e
Djimy SURLIN <158576986+sdjim15@users.noreply.github.com>
2024-07-31 23:16:37 -0400

    resolution de plusieurs points du Tp 3

--- app/src/main/java/fr/gobelins/dmi1/ComputeActivity.kt
+++ app/src/main/java/fr/gobelins/dmi1/ComputeActivity.kt
@@ -1,15 +1,43 @@
 package fr.gobelins.dmi1
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class ComputeActivity : AppCompatActivity() {
+    lateinit var edOperande1: EditText
+    lateinit var edOperande2: EditText
+    lateinit var txtAffichage: TextView
+    lateinit var btnCalculer: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.compute_activity)
+        //val operation = intent.getStringExtra("operation") ?: "ADD"
+        edOperande1 = findViewById(R.id.first_operand)
+        edOperande2 = findViewById(R.id.second_operand)
+        txtAffichage = findViewById(R.id.affichage)
+        btnCalculer = findViewById(R.id.btn_calculer)
 
-        val operation = intent.getStringExtra("operation") ?: "ADD"
-
-
+        btnCalculer.setOnClickListener {
+            val operande1 =
+                edOperande1.text.toString().toDoubleOrNull() ?: return@setOnClickListener run {
+                    // edOperande1.error
+                    Toast.makeText(this, "Veuillez rentrer une donnee valide", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            val operande2 =
+                edOperande2.text.toString().toDoubleOrNull() ?: return@setOnClickListener run {
+                    // edOperande1.error
+                    Toast.makeText(this, "Veuillez rentrer une donnee valide", Toast.LENGTH_SHORT)
+                        .show()
+                }
+                val somme = operande1 + operande2
+            txtAffichage.text = somme.toString()
+        }
     }
 }
--- app/src/main/java/fr/gobelins/dmi1/MainActivity.kt
+++ app/src/main/java/fr/gobelins/dmi1/MainActivity.kt
@@ -1,11 +1,23 @@
 package fr.gobelins.dmi1
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import fr.gobelins.dmi1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    lateinit var buttonHomeSp : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //buttonHomeSp = findViewById(R.id.btn_home_compute)
+        binding.btnHomeCompute.setOnClickListener{
+            val intent = Intent(this,ComputeActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
